7d37c3483e7334bda2964de4d73c84d511e190ae
Nils Koelling <nk@nk.gl>
2016-08-03 16:49:34 +0100

    version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "dnacol",
-    version = "0.2.1",
+    version = "0.2.2",
     packages = find_packages(),
     scripts = ['dnacol'],
 
