89e3203770ee87f241e8fada66f9ae3bd9cb19df
nk <nk@nk.gl>
2017-07-29 16:40:00 +0100

    fix up package structure, setup script

--- setup.py
+++ setup.py
@@ -2,19 +2,26 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
+
+version = "0.3"
 setup(
     name = "dnacol",
-    version = "0.2.2",
+    version = version,
     packages = find_packages(),
-    scripts = ['dnacol'],
 
     # metadata for upload to PyPI
-    author = "Nils Kölling",
+    author = "Nils Koelling",
     author_email = "git@nk.gl",
     description = "Pipe output through this script to color DNA/RNA bases in the terminal",
     license = "MIT",
-    keywords = "dnacol rnacol rna dna color",
+    keywords = "dnacol rnacol rna dna color colour bases console terminal stdout",
     url = "https://github.com/koelling/dnacol/",
+    download_url="https://github.com/koelling/dnacol/archive/%s.tar.gz" % version,
+    platforms=["any"],
+
+    entry_points={
+        'console_scripts': ['dnacol = dnacol.dnacol:main']
+    },
 
     # could also include long_description, download_url, classifiers, etc.
 )
