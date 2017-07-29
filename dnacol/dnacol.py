99968d2559255a4f413f7a57a0e2ff70ad37b672
nk <nk@nk.gl>
2017-07-29 22:35:45 +0100

    refactor quality writing code, quality colors, sam auto detection

--- dnacol/dnacol.py
+++ dnacol/dnacol.py
@@ -4,6 +4,7 @@
 import sys
 import re
 import argparse
+import math
 
 foreground_color = 97
 base_colors = {
@@ -53,28 +54,25 @@ def detect_quality_encoding(raw_quality_scores):
 def set_color(color_code):
     sys.stdout.write('\033[{}m'.format(color_code))
 
-def print_colored_quality_characters(wide, characters, phred_quality_base, raw_scores = None):
+def write_colored_quality_characters(wide, characters, phred_quality_base):
     #calculate score as ASCII code minus base
-    if raw_scores is not None:
-        scores = [score - phred_quality_base for score in raw_scores]
-    else:
-        scores = [ord(character) - phred_quality_base for character in characters]
+    scores = [ord(character) - phred_quality_base for character in characters]
 
     #truncate the scores to make sure we don't get values <0 or >40
     scores = [0 if score < 0 else 40 if score > 40 else score for score in scores]
 
-    #print out the colored characters
+    #write out the colored characters
     previous_color = None
     for quality_score, quality_character in zip(scores, characters):
         #determine the right color for this score
-        my_color = quality_colors[int(round(float(quality_score) / 40 * (len(quality_colors)-1)))]
+        my_color = quality_colors[int(math.floor(float(quality_score) / 41 * (len(quality_colors))))]
 
         #only change color if we have to
         if my_color != previous_color:
             set_color(my_color)
             previous_color = my_color
 
-        #print the original character
+        #write the original character
         if wide:
             sys.stdout.write(' {} '.format(quality_character))
         else:
@@ -83,13 +81,13 @@ def print_colored_quality_characters(wide, characters, phred_quality_base, raw_s
     #reset color at the end
     set_color(0)
 
-def print_colored_dna_characters(wide, characters):
+def write_colored_dna_characters(wide, characters):
     previous_base = ''                
     for base in characters:
         if base in base_colors:
             #set color, but only if we have to
             if previous_base == '':
-                #make sure we have the correct foreground color
+                #make sure we have the correct foreground color (need to do this inside the loop since we may have reset below)
                 set_color(foreground_color)
 
             if base != previous_base:
@@ -114,13 +112,17 @@ def main(argv=None):
     #prepare description and epilog texts (shown for --help) 
     help_description = 'This script reads lines from STDIN or a file, identifies strings of DNA/RNA and phred-encoded quality scores, and writes colored output to STDOUT.'
     help_description += ' When a file name is provided, files ending in .gz will be decompressed on the fly and the file format will be detected based on the extension.'
-    help_description += ' In addition, files in SAM or FASTQ format will also be detected based on their content.'
+    help_description += ' Data in SAM or FASTQ format can also be detected when piped through STDIN.'
 
     help_epilog = ''    
     help_epilog += 'examples:\n  head reads.fastq | {}\n  {} genome.fa\n\n'.format(sys.argv[0], sys.argv[0])
-    help_epilog += 'color scheme: '
+    help_epilog += 'base color scheme:'
     for base in ['A', 'T', 'C', 'G', 'U', 'N']:
         help_epilog += ' \033[{}m\033[{}m {} \033[0m'.format(foreground_color, base_colors[base], base)
+    help_epilog += '\n'
+    help_epilog += 'quality color scheme:'
+    for quality_score in range(41):
+        help_epilog += ' \033[{}m{:02d}\033[0m'.format(quality_colors[int(math.floor(float(quality_score) / 41 * (len(quality_colors))))], quality_score)
 
     #parse command-line arguments
     parser = argparse.ArgumentParser(description = help_description,
@@ -136,7 +138,7 @@ def main(argv=None):
         help="debug mode (raise exceptions)",
         action='store_true')
     parser.add_argument("file", nargs="?",
-        help="file name to read (default: read from STDIN)",
+        help="file name to read (default: - = read from STDIN)",
         default='-')
     args = parser.parse_args(argv[1:])
 
@@ -184,8 +186,9 @@ def main(argv=None):
                 if line.startswith('#CHROM\tPOS\tID\tREF\tALT'):
                     args.format = 'vcf'
 
-            #auto-detect FASTQ format
-            if args.format == 'auto' and args.file == '-':                
+            #auto-detect format based on file contents
+            if args.format == 'auto' and args.file == '-':
+                #auto-detect FASTQ format
                 if possible_fastq:
                     #first row should start with @ (read identifier)
                     if line_counter == 0 and not line.startswith('@'):
@@ -201,76 +204,49 @@ def main(argv=None):
                         #at this point we can assume we have fastq
                         args.format = 'fastq'
 
+                #auto-detect SAM format
+                if possible_sam:
+                    #skip headers
+                    if not line.startswith('@'):
+                        #try to see if this is in SAM format
+                        if re.search('^[!-?A-~]{1,254}'+
+                            '\t[0-9]+'+
+                            '\t.+'+
+                            '\t[0-9]+'+
+                            '\t[0-9]+'+
+                            '\t(\*|([0-9]+[MIDNSHPX=])+)'+
+                            '\t.+'+
+                            '\t[0-9]+'+
+                            '\t[0-9-]+'+
+                            '\t(\*|[A-Za-z=.]+)'
+                            '\t.+(\t|$)', line
+                        ):
+                            args.format = 'sam'
+                        else:
+                            #make sure we don't try this for every line
+                            possible_sam = False
+
             #only parse specific lines
             do_search = True
-            do_color_quality = False
             if args.format == 'fastq':
-                #only color the second line per read (sequence)
-                do_search = line_counter % 4 == 1
-                #we can also color the quality scores
-                do_color_quality = line_counter % 4 == 3
+                #only color the second and fourth line per read (sequence, quality)
+                do_search = line_counter % 4 == 1 or line_counter % 4 == 3
             elif args.format == 'fasta':
-                #do not color id rows
+                #ignore id lines
                 do_search = not line.startswith('>')
             elif args.format == 'vcf':
-                #ignore comments
+                #ignore comment lines
                 do_search = not line.startswith('#')
             elif args.format == 'sam':
-                #ignore header
+                #ignore header lines
                 do_search = not line.startswith('@')
-                #quality should always be phred+33
+
+                #quality should always be phred+33, according to SAM format specification
                 phred_quality_base = 33
 
-            #keep track of line (for fastq files)
-            line_counter += 1
-
             #process the line
-            if do_color_quality:
-                #get ascii value for each character
-                quality_characters = line.rstrip('\r\n')
-                raw_quality_scores = [ord(character) for character in quality_characters]
-
-                #try to detect quality base
-                if phred_quality_base is None:
-                    phred_quality_base = detect_quality_encoding(raw_quality_scores)
-                    if phred_quality_base == -1:
-                        sys.stderr.write('Detected FASTQ format but encountered unexpecte quality score range: min = {}, max = {}.\n'.format(
-                            min(quality_scores), max(quality_scores)))
-
-                #only proceed if we have a proper phred base
-                if phred_quality_base > -1:
-                    #print the colored scores
-                    print_colored_quality_characters(args.wide, quality_characters, phred_quality_base, raw_scores = raw_quality_scores)
-
-                    #print rest of line (eg. \n)
-                    if len(quality_characters) < len(line):
-                        sys.stdout.write(line[len(quality_characters):])
-                #otherwise just print out the line
-                else:
-                    sys.stdout.write(line)
-            elif do_search:
-                #auto-detect SAM format
-                if args.format == 'auto' and args.file == '-':
-                    if possible_sam:
-                        #skip headers
-                        if not line.startswith('@'):
-                            #try to see if this is in SAM format
-                            if re.search('^[!-?A-~]{1,254}'+
-                                '\t[0-9]+'+
-                                '\t.+'+
-                                '\t[0-9]+'+
-                                '\t[0-9]+'+
-                                '\t(\*|([0-9]+[MIDNSHPX=])+)'+
-                                '\t.+'+
-                                '\t[0-9]+'+
-                                '\t[0-9-]+'+
-                                '\t(\*|[A-Za-z=.]+)'
-                                '\t.+(\t|$)', line
-                            ):
-                                args.format = 'sam'
-                            else:
-                                possible_sam = False
-
+            if do_search:
+                #figure out which parts of the line to color
                 if args.format == 'sam':
                     #only color column #10 (SEQ)
                     matches = find_column_spans(line, {10: 'dna', 11: 'quality'})
@@ -278,8 +254,14 @@ def main(argv=None):
                     #only color columns #4+5 (REF+ALT)
                     matches = find_column_spans(line, {4: 'dna', 5: 'dna'})
                 elif args.format == 'fastq' or args.format == 'fasta':
+                    match_type = 'dna'
+                    if args.format == 'fastq':
+                        if line_counter % 4 == 3:
+                            match_type = 'quality'
+
                     #color the entire line, except the \n at the end
-                    matches = [ (0, line.rfind('\n'), 'dna') ]
+                    line_end = line.find('\n')
+                    matches = [ (0, line_end if line_end > 0 else len(line), match_type) ]
                 else:
                     #search for strings of DNA/RNA in text
                     pattern = '\\b[ACGTUN]+\\b'
@@ -292,9 +274,20 @@ def main(argv=None):
 
                     #determine whether this is a quality or DNA string
                     if match_type == 'quality':
-                        print_colored_quality_characters(args.wide, line[match_start:match_end], phred_quality_base)
+                        #try to detect quality base
+                        if phred_quality_base is None:
+                            raw_quality_scores = [ord(character) for character in line[match_start:match_end]]
+                            phred_quality_base = detect_quality_encoding(raw_quality_scores)
+                            if phred_quality_base == -1:
+                                sys.stderr.write('Detected FASTQ format but encountered unexpected quality score range: min = {}, max = {}.\n'.format(
+                                    min(raw_quality_scores), max(raw_quality_scores)))
+
+                        if phred_quality_base >= 0:
+                            write_colored_quality_characters(args.wide, line[match_start:match_end], phred_quality_base)
+                        else:
+                            sys.stdout.write(line[match_start:match_end])
                     else:
-                        print_colored_dna_characters(args.wide, line[match_start:match_end])
+                        write_colored_dna_characters(args.wide, line[match_start:match_end])
 
                     #remember how far we have written already
                     last_match_end = match_end
@@ -306,6 +299,9 @@ def main(argv=None):
                 sys.stdout.write(line[last_match_end:])
             else:
                 sys.stdout.write(line)
+
+            #keep track of line (for fastq files)
+            line_counter += 1
 
         #flush output
         sys.stdout.flush()
